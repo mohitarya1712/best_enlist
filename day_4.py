e091308ca11101c54d03c34268b351c6d52d9b0a
mohitarya1712 <85779907+mohitarya1712@users.noreply.github.com>
2021-06-15 11:21:43 +0530

    Add files via upload

---
+++ day_4.py
@@ -0,0 +1,40 @@
+#exercise: day_4
+
+'''1.Create three variables (a,b,c) to same value of any integer & 
+do the following
+a) Divide a by 10
+b) Multiply b by 50
+c) Add c value by 60'''
+
+a = b = c = 70
+print(a,b,c)
+print(a/10)
+print(b*20)
+print(c+60)
+
+'''2.Create a String variable of 5 characters and replace the 3rd
+character with G'''
+x = "abcde"
+y = x.replace("c","G",3)
+print(x)
+print(y)
+
+'''3.Create two values (a,b) of int,float data type & convert the 
+vise versa, Hint : convert a from int to float datatype & b from 
+float to int datatype'''
+a = 7
+b = 7.7
+print(type(a))
+print(a)
+print(type(b))
+print(b)
+
+x = float(a)
+y = int(b)
+print(type(x))
+print(x)
+print(type(y))
+print(y)
+
+
+
